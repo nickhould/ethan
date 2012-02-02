11b2976ad7add118860b52ad3a4f5846e93efc3d
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2012-02-01 19:46:23 -0500

    Added "Darkness".

--- config.rb
+++ config.rb
@@ -1,12 +1,12 @@
 project_type = :stand_alone
 
 http_path = "/"
-http_images_path = "/images"
+http_images_path = "/img"
 http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+css_dir = "public/css"
 
 sass_dir = "sass"
-images_dir = "source/images"
+images_dir = "source/img"
 fonts_dir = "source/fonts"
 
 line_comments = false
